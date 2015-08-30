2738c1e4c8c40bffe6b75a5bedd663dacbab2cc6
ariely78 <ariely.y@gmail.com>
2015-08-30 23:49:07 +0300

    Update GraphAssertResultsTest.java

--- src/clique_algo/GraphAssertResultsTest.java
+++ src/clique_algo/GraphAssertResultsTest.java
@@ -8,12 +8,12 @@ public class GraphAssertResultsTest {
 
     @Test
     public void testGraphStringDoubleInt() {
-        Graph G= new Graph("test1Small.csv");
+        Graph G= new Graph("testBig.csv");
 
 
         // assert statements
-        assertEquals("Edges must be 7", 7, G.E());
-        assertEquals("Vertices must be 9", 9, G.V());
+        assertEquals("Edges must be 2407", 2407, G.E());
+        assertEquals("Vertices must be 1728",1728, G.V());
         //assertEquals("Minimal Clique must be 5", 5,G.min());
         // assertEquals("Threshold must be 0.1", 0.1, G.TH());
         //fail("Not yet implemented");
